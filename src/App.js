d0730c6f954d4fe3a56da6f2d116af9896573b8e
arthurwkm <arthursantosvs@gmail.com>
2020-06-23 23:13:10 -0300

    components pt 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     
-    <div className="App">
+    <div>
       <Navbar dark color = "primary">    
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>  
