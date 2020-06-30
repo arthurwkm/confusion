0b8619febf4eed20ef738fe5eb431352ae31d75c
arthurwkm <arthursantosvs@gmail.com>
2020-06-29 23:09:59 -0300

    react components pt2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+//importando a constante
 
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+      //definindo a variavel para a constante importada
+    };
+  }
+
+  render(){
   return (
     
     <div>
@@ -14,9 +26,10 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>  
         </div> 
       </Navbar>
-      <Menu />
+      <Menu dishes = {this.state.dishes}/>
     </div>
   );
 }
-
+//passando a variavel para o componente menu
+}
 export default App;
