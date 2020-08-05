c12c4120ae5b58329b3c1fdf320c52a1d13791ed
Arthur <arthursantosvs@gmail.com>
2020-08-05 19:42:52 -0300

    intro to redux

--- confusion4/src/App.js
+++ confusion4/src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
 import { BrowserRouter } from 'react-router-dom';
 //importando a constante
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
 
 class App extends Component {
 
-  callbackFunction(childData) {
-    this.setState({ selectedDish: childData })
-  }
+
 
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Main />
-        </div>
-      </BrowserRouter>
-
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
   //passando a variavel para o componente menu
--- confusion4/src/components/MainComponent.js
+++ confusion4/src/components/MainComponent.js
@@ -6,25 +6,25 @@ import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
-import { Switch, Route, Redirect } from 'react-router-dom';
 
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+
+const mapStateToProps = state => {
+  return{
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }   
+}
 
 class Main extends Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      leaders: LEADERS,
-      promotions: PROMOTIONS,
-      selectedDish: null
-      //definindo a variavel para a constante importada
-    };
+   
   }
 
   
@@ -33,16 +33,16 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-        <Home  dish={this.state.dishes.filter((dish) => dish.featured === true)[0]}
-        promo={this.state.promotions.filter((promo) => promo.featured === true)[0]}
-        leader={this.state.leaders.filter((leader) => leader.featured === true)[0]} />
+        <Home  dish={this.props.dishes.filter((dish) => dish.featured === true)[0]}
+        promo={this.props.promotions.filter((promo) => promo.featured === true)[0]}
+        leader={this.props.leaders.filter((leader) => leader.featured === true)[0]} />
       );
     }
 
     const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
     };
 
@@ -52,11 +52,11 @@ class Main extends Component {
       <Header/>
       <Switch>
         <Route path="/home" component={HomePage} />
-        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+        <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/>} />
         {/* se nenhuma route der match vá para o redirect */}
         <Route path = "/menu/:dishId" component = {DishWithId}/>
         <Route exact path="/contactus" component={() => <Contact/>} />
-        <Route exact path="/aboutus" component={() => <About leaders = {this.state.leaders}/>} /> 
+        <Route exact path="/aboutus" component={() => <About leaders = {this.props.leaders}/>} /> 
         <Redirect to="/home"/>
       </Switch>
       <Footer/>
@@ -65,4 +65,5 @@ class Main extends Component {
 }
 //passando a variavel para o componente menu
 }
-export default Main;
+
+export default withRouter(connect(mapStateToProps)(Main));
